77a9c816b9c2160e66889c4918b12e01c489cc2d
Jens Ritter <jens.ritter@gmail.com>
2022-02-06 22:09:40 +0100

    template-forms sind "fertig" .. hm. .. fühlt sich chaotisch an. ( ist aber fix )

--- modules/forms/src/app/app.component.ts
+++ modules/forms/src/app/app.component.ts
@@ -14,11 +14,42 @@ export class AppComponent {
 
   genders: string[] = ['male', 'femail']
 
+
+  submitted = false;
+  user = {
+    username: "",
+    email: "",
+    secrectQuestion: "",
+    answer: "",
+    gender: ""
+  }
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm?.setValue({
+    //   userdata: {
+    //     username: suggestedName,
+    //     email : 'emptyString'
+    //   },
+    //   secret: "pet",
+    //   questionAnswer: '',
+    //   gender:'male',
+    // });
+
+    // partial set value
+    this.signupForm?.form.patchValue({
+      userdata: {username: suggestedName}
+    });
   }
 
   onSubmit(element: NgForm) {
-    console.log(this.signupForm);
+    this.user.username = this.signupForm?.value.userdata.username;
+    this.user.email = this.signupForm?.value.userdata.email;
+    this.user.secrectQuestion = this.signupForm?.value.secret;
+    this.user.answer = this.signupForm?.value.questionAnswer;
+    this.user.gender = this.signupForm?.value.gender;
+    this.submitted = true;
+
+    this.signupForm?.reset();
   }
 }
