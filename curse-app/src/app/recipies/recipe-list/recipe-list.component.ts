222b13f4f4c0511a09dd1966b5a105e35c125db7
Jens Ritter <jens.ritter@gmail.com>
2022-02-06 22:09:36 +0100

    plain curse app

--- curse-app/src/app/recipies/recipe-list/recipe-list.component.ts
+++ curse-app/src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recipie} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipies: Recipie[] = [
+    new Recipie("Default", "Default Recipie", "https://www.taste-and-more.ch/media/image/5b/10/66/Mie-Noodels-Asiatisch-by-taste-and-more-ch_600x600.jpg")
+  ];
 
   constructor() {
   }
