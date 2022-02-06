d78a050d9c37aa29658d1889e7ddeafd743def22
Jens Ritter <jens.ritter@gmail.com>
2022-02-06 22:09:38 +0100

    curse: haupt-routes gesetzt, und bei allen a-tags das href gelöscht, damit dies dann durch routen / js ersetzt werden kann.
    das alles soll dazu führen, das der browser die seite niemals neu läd.

--- curse-app/src/app/app-routing.module.ts
+++ curse-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
+import {RecipiesComponent} from "./recipies/recipies.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/recipies", pathMatch: "full"},
+  {path: "recipies", component: RecipiesComponent},
+  {path: "shopping-list", component: ShoppingListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- curse-app/src/app/app.component.ts
+++ curse-app/src/app/app.component.ts
@@ -7,9 +7,4 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
 
-  currentPage = "recipies";
-
-  onSwitchPage(event: string) {
-    this.currentPage = event;
-  }
 }
