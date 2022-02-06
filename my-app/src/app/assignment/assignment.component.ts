286b7c45c381f7ea874070abc9466bdc24023140
Jens Ritter <jens.ritter@gmail.com>
2022-02-06 22:09:35 +0100

    einfache directives ( if / for )

--- my-app/src/app/assignment/assignment.component.ts
+++ my-app/src/app/assignment/assignment.component.ts
@@ -6,10 +6,27 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./assignment.component.scss']
 })
 export class AssignmentComponent implements OnInit {
+  name: string = "";
+  servers: string[] = [];
+  alert: string = "";
+  alertIsOk = true;
+
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  onBtnClick() {
+    this.servers.push(this.name);
+    this.alertIsOk = Math.random() > 0.5;
+    console.log("Alert is : ", this.alertIsOk);
+    this.name = "";
+    this.alert = this.servers.join(",");
+  }
+
+  serverListUsed() {
+    return this.servers.length > 0;
   }
 
 }
